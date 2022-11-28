6ef251ebb1451555ef1e12313daee06cdd8a0317
slimen1985 <vadart1985@gmail.com>
2022-11-28 16:38:54 +0300

    lesson3

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Women(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -5,7 +5,6 @@ from .views import *
 urlpatterns = [
 
     path('', index, name='home'),  # http://127.0.0.1:8000/women/
-    path('cats/<int:catid>/', categories),  # http://127.0.0.1:8000/women/cats/
-    re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
+    path('about/', about, name='about'),
 
 ]
